6bbc02375349a4eed72b394fe42665d215708fe2
Ka Hei Chan <kaheichan1@gmail.com>
2026-03-29 15:40:10 -0400

    Togglable lightning

--- cat-witch/scenes/entities/rain.gd
+++ cat-witch/scenes/entities/rain.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 var cat: Node2D
+@export var lightning_on = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$ColorRect.hide()
+	$RainCollision.lightning_on = lightning_on
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- cat-witch/scenes/entities/rain_collision.gd
+++ cat-witch/scenes/entities/rain_collision.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 var lightningObject = load("res://scenes/entities/lightning.tscn")
 var lightning
+var lightning_on = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,16 +16,18 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.wet = true
 		get_parent().cat = body
-		lightning = lightningObject.instantiate()
-		lightning.local_rain = self
-		lightning.local_body = body
-		add_sibling(lightning)
-		$LightningTimer.start()
+		if lightning_on:
+			lightning = lightningObject.instantiate()
+			lightning.local_rain = self
+			lightning.local_body = body
+			add_sibling(lightning)
+			$LightningTimer.start()
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.wet = false
-		if lightning:
-			lightning.queue_free()
-		$LightningTimer.stop()
-		$LightningTimer.set_wait_time(5)
+		if lightning_on:
+			if lightning:
+				lightning.queue_free()
+			$LightningTimer.stop()
+			$LightningTimer.set_wait_time(5)
