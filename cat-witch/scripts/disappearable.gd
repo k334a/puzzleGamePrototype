4f9e05618230adf98e80cfc84169230a7495a8bd
Ka Hei Chan <kaheichan1@gmail.com>
2026-04-04 18:17:32 -0400

    Sewer things

---
+++ cat-witch/scripts/disappearable.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+var player_in
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if player_in:
+		if player_in.is_on_floor():
+			self.queue_free()
+
+func _on_body_entered(body: Node2D) -> void:
+	if body:
+		if body.is_in_group("player"):
+			print("HEJ")
+			player_in = body
+
+func _on_body_exited(_body: Node2D) -> void:
+	player_in = null
