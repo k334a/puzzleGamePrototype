4b5d9b6543ca954e590756be71651a121b23b9da
Ka Hei Chan <kaheichan1@gmail.com>
2026-04-04 23:32:53 -0400

    Final Level

--- cat-witch/scenes/entities/cat.gd
+++ cat-witch/scenes/entities/cat.gd
@@ -293,7 +293,7 @@ func readSpell(left_input, right_input, down_input, up_input, index):
 	
 	var spell_list = {
 		0: func(): wind(lookVector); spellCooldowns[index] = 2,
-		1: func(): freeze(); spellCooldowns[index] = 1,
+		1: func(): freeze(); spellCooldowns[index] = 7,
 		2: func(): light(); spellCooldowns[index] = 1,
 		3: func(): spellCooldowns[index] = plant(),
 	}
