2e6c2dc1487c257c1cc90aff94e8e16dea9dc2b3
Adrian <adriansnhagyard@gmail.com>
2026-04-06 07:40:43 -0400

    Lightning sound to rain

--- cat-witch/scenes/entities/rain.gd
+++ cat-witch/scenes/entities/rain.gd
@@ -33,6 +33,7 @@ func _on_lightning_timer_timeout() -> void:
 	var direction = target - origin
 	var distance = direction.length()
 	
+	$RainCollision.play_lightning_sound()
 	$ColorRect.global_position = origin
 	$ColorRect.size = Vector2(50, distance)
 	$ColorRect.rotation = direction.angle() - PI/2
